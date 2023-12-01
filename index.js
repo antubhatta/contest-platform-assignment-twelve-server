c893a0a43bed9cb4ab1d1fb5cf23f45e805ea8e8
antubhatta <antubhatta09@gmail.com>
2023-12-02 01:44:42 +0600

    Implemented api for contest

--- index.js
+++ index.js
@@ -25,118 +25,175 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const db = client.db("contestDB");
+    const db = client.db('contestDB');
 
+    const usersCollection = db.collection('users');
+    const contestCollection = db.collection('contests');
+    
+    app.use(express.json());
+    
     // AUTHENTICATION
-
+    
     // generate a new token
-    app.post("/jwt", async (req, res) => {
+    app.post('/jwt', async (req, res) => {
       const body = req.body;
       const token = jwt.sign(body, process.env.JWT_SECRET, {
-        expiresIn: "1h",
+        expiresIn: '1h',
       });
       res.send({ token });
     });
-
+    
     // verify the token
     const verifyToken = (req, res, next) => {
       if (!req.headers.authorization) {
-        return res.status(401).send({ message: "Unauthorized Access" });
+        return res.status(401).send({ message: 'Unauthorized Access' });
       }
-
-      const token = req.headers["authorization"].split(" ")[1] || null;
+    
+      const token = req.headers.authorization.split(' ')[1] || null;
       if (!token) {
-        return res.status(401).send({ message: "Unauthorized Access" });
+        return res.status(401).send({ message: 'Unauthorized Access' });
       }
-
+    
       // If there is a token, verify it
       jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
-          return res.status(401).send({ message: "Unauthorized Access" });
+          return res.status(401).send({ message: 'Unauthorized Access' });
         }
         req.decoded = decoded;
         next();
       });
     };
 
-    // verify the admin
-    const verifyAdmin = async (req, res, next) => {
-      const email = req.decoded.email;
-      const query = { email };
-      const result = await usersCollection.findOne(query);
-      const isAdmin = result && result.role === "admin";
-      if (!isAdmin) {
-        return res.status(403).send({ message: "Forbidden Access" });
-      }
-      next();
-    };
+        // Controller
+        const contestController = {
+          getAllContests: async (req, res) => {
+              try {
+                const searchText = req.query.search || "";
+          
+                const result = await contestCollection.aggregate([
+                  {
+                    $match: {
+                      status: "accepted",
+                      type: { $regex: searchText, $options: "i" },
+                    },
+                  },
+                  {
+                    $project: {
+                      title: 1,
+                      type: 1,
+                      image: 1,
+                      description: 1,
+                      participantsCount: { $size: "$participants" },
+                    },
+                  },
+                  {
+                    $sort: { participantsCount: -1 },
+                  },
+                ]).toArray();
+          
+                res.status(200).json(result);
+              } catch (error) {
+                res.status(500).json({ error: error.message });
+              }
+            },
+          
+            getContestById: async (req, res) => {
+              try {
+                const id = req.params.id;
+          
+                const result = await contestCollection.findOne({ _id: toObjectId(id) });
+                const winner = await usersCollection.findOne({ _id: toObjectId(result.winner) });
+          
+                result.winner = winner;
+          
+                res.send(result);
+              } catch (error) {
+                res.status(500).send(error);
+              }
+            },
+          
+            getContestByIdForCreators: async (req, res) => {
+              const contestId = req.params.contestId;
+              const creatorId = req.params.creatorId;
+          
+              try {
+                const contest = await contestCollection.findOne({ _id: toObjectId(contestId) });
+                const creator = await usersCollection.findOne({ _id: toObjectId(creatorId) });
+          
+                if (!contest) {
+                  return res.status(404).send({ message: "Contest not found" });
+                }
+          
+                // Check if the logged-in user is the creator of the contest
+                if (contest.creator.toString() !== creatorId) {
+                  return res.status(403).send({ message: "Access denied" });
+                }
+          
+                const tasks = await taskCollection.find({ contestId: contestId }).toArray();
+          
+                const participantsWithTasks = contest.participants.map((participant) => {
+                  const participantTask = tasks.find((task) =>
+                    task.participantId.equals(participant)
+                  );
+          
+                  return {
+                    _id: participant,
+                    name: participantTask ? participantTask.task : null,
+                  };
+                });
+          
+                const formattedContest = {
+                  _id: contest._id,
+                  title: contest.title,
+                  description: contest.description,
+                  deadline: contest.deadline,
+                  prizeMoney: contest.prizeMoney,
+                  winner: contest.winner,
+                  participants: participantsWithTasks,
+                };
+          
+                res.status(200).send(formattedContest);
+              } catch (error) {
+                res
+                  .status(500)
+                  .send({ message: error?.message || "Internal server error" });
+              }
+            },
+          
+            getPopularContests: async (req, res) => {
+              try {
+                const result = await contestCollection.aggregate([
+                  { $match: { status: "accepted" } },
+                  {
+                    $project: {
+                      title: 1,
+                      type: 1,
+                      image: 1,
+                      description: 1,
+                      participantsCount: { $size: "$participants" },
+                    },
+                  },
+                  {
+                    $sort: { participantsCount: -1 },
+                  },
+                  { $limit: 6 },
+                ]).toArray();
+          
+                res.status(200).json(result);
+              } catch (error) {
+                res.status(500).json({ error: error.message });
+              }
+            },
+          }
+    
+          app.get('/contests/', contestController.getAllContests);
+          app.get('/contests/:id', contestController.getContestById);
+          app.get('/contests/popular', contestController.getPopularContests);
+          app.get('/contests/admin', verifyToken, contestController.getAllContestsForAdmin);
+          app.get('/contests/creator/:creatorId',  contestController.getContestByCreator);
+    
 
-    // payment
-    app.post("/create-payment-intend", async (req, res) => {
-      const { total } = req.body;
 
-      // create a payment intent with the order amount and currency
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: parseInt(total * 100),
-        currency: "usd",
-        payment_method_types: ["card"],
-      });
-
-      res.send({
-        clientSecret: paymentIntent.client_secret,
-      });
-    });
-
-    // Users Collection
-    const usersCollection = db.collection("users");
-
-    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
-      const result = await usersCollection.find({}).toArray();
-      res.send(result);
-    });
-
-    // check admin
-    app.get("/users/admin", verifyToken, async (req, res) => {
-      const email = req.query.email;
-      const query = { email };
-      const result = await usersCollection.findOne(query);
-      let isAdmin = false;
-      if (result && result.role === "admin") {
-        isAdmin = true;
-      }
-      res.send({ isAdmin });
-    });
-
-    app.post("/users", async (req, res) => {
-      const body = req.body;
-
-      const isAlreadyExist = await usersCollection.findOne({
-        email: body.email,
-      });
-      if (isAlreadyExist) {
-        return res.send({ message: "User already exists" });
-      }
-
-      const result = await usersCollection.insertOne(body);
-      res.send(result);
-    });
-
-    app.patch("/users/:id", verifyToken, verifyAdmin, async (req, res) => {
-      const id = req.params.id;
-      const body = req.body;
-      const query = { _id: new ObjectId(id) };
-      const update = { $set: body };
-      const options = { upsert: false };
-      const result = await usersCollection.updateOne(query, update, options);
-      res.send(result);
-    });
-
-    app.delete("/users/:id", verifyToken, verifyAdmin, async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await usersCollection.deleteOne(query);
-      res.send(result);
-    });
 
     
 
